ae4d2e59e381fcda7473236fcc19b86396b47756
Mark Nunberg <mnunberg@haskalah.org>
2012-12-14 15:50:54 -0800

    Tagged for 1.0 release

--- lib/JSON/SL.pm
+++ lib/JSON/SL.pm
@@ -16,7 +16,7 @@ use base qw(Exporter);
 our @EXPORT_OK = qw(decode_json unescape_json_string);
 
 BEGIN {
-    $VERSION = '0.08';
+    $VERSION = '1.0.0';
     require XSLoader;
     XSLoader::load(__PACKAGE__, $VERSION);
 }
