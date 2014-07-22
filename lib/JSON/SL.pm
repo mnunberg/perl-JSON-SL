3e33df65c1c64479f41906ce809a3fd7ffa07c24
Mark Nunberg <mnunberg@haskalah.org>
2014-07-22 11:14:16 -0700

    Bump to version 1.0.2

--- lib/JSON/SL.pm
+++ lib/JSON/SL.pm
@@ -16,7 +16,7 @@ use base qw(Exporter);
 our @EXPORT_OK = qw(decode_json unescape_json_string);
 
 BEGIN {
-    $VERSION = '1.0.1';
+    $VERSION = '1.0.2';
     require XSLoader;
     XSLoader::load(__PACKAGE__, $VERSION);
 }
